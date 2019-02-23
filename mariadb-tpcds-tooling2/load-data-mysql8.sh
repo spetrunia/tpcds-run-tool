abe107d334d4bc37cca1c3692f09764c9dbde76a
Sergei Petrunia <psergey@askmonty.org>
2019-02-23 18:49:11 +0000

    mysql 8 fixes

---
+++ mariadb-tpcds-tooling2/load-data-mysql8.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+# This expects to find the generated data in data-for-mysql
+
+# Load the dataset
+MYSQL="./mysql-8.0/runtime_output_directory/mysql"
+SOCKET="--socket=/tmp/mysql20.sock"
+MYSQL_USER="-uroot"
+MYSQL_ARGS="$MYSQL_USER $SOCKET"
+
+$MYSQL $MYSQL_ARGS -e "create database tpcds"
+$MYSQL $MYSQL_ARGS tpcds < mariadb-tpcds-tooling2/ddl/tables.sql
+$MYSQL $MYSQL_ARGS tpcds < mariadb-tpcds-tooling2/ddl/indexes.sql
+
+DATA_FILES_DIR="`pwd`/data-for-mysql/"
+
+bash mariadb-tpcds-tooling2/ddl/load-mysql.sql.sh $DATA_FILES_DIR > load-mysql.sql
+
+$MYSQL $MYSQL_ARGS tpcds < load-mysql.sql
+
+
---
+++ mariadb-tpcds-tooling2/run-queries-mysql8.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+# TODO: factor out the config soemwhere, and then this is the same as with MariaDB
+MYSQL="./mysql-8.0/runtime_output_directory/mysql"
+SOCKET="--socket=/tmp/mysql20.sock"
+MYSQL_USER="-uroot"
+MYSQL_ARGS="$MYSQL_USER $SOCKET"
+
+ls queries-for-mysql/*.sql | while read a ; do
+  $MYSQL $MYSQL_ARGS tpcds < $a |  tee benchmark-output-raw.txt
+done
+
+#grep LOG_END benchmark-output-raw.txt | sort | awk '//{printf ("%s %s\n" , $2, $3) }'
+
