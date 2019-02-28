2d680311bfb1a087948e75746bf0534e431a1844
Sergei Petrunia <psergey@askmonty.org>
2019-02-27 22:34:58 -0500

    More cleanups (not finished)

--- mariadb-tpcds-tooling2/20-run-queries-pg.sh
+++ mariadb-tpcds-tooling2/20-run-queries-pg.sh
@@ -7,12 +7,13 @@ ls queries-for-pg/*.sql | while read a ; do
   ./postgresql-11.2-inst/bin/psql tpcds < $a |  tee benchmark-output-raw.txt
 done
 
-cat << END
+(./postgresql-11.2-inst/bin/psql tpcds  | tee pg-result.txt) << END
 select 
   query_stream, count(*), sum(query_time_ms) 
 from
   my_tpcds_result
 group by
   query_stream;
-END | ./postgresql-11.2-inst/bin/psql tpcds  | tee pg-result.txt
+END 
+
 
---
+++ mariadb-tpcds-tooling2/20-run-queries.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [ ! -f mysql-config.sh ] ; then
+  echo "Cannot find mysql-config.sh - did you setup the db?"
+  exit 1;
+fi
+
+source mysql-config.sh
+echo "Running data using $MYSQL $MYSQL_ARGS";
+
+ls queries-for-mysql/*.sql | while read a ; do
+  $MYSQL $MYSQL_ARGS tpcds < $a |  tee benchmark-output-raw.txt
+done
+
+($MYSQL $MYSQL_ARGS tpcds | tee mysql-result.txt) << END
+select 
+  query_stream, count(*), sum(query_time_ms) 
+from
+  my_tpcds_result
+group by
+  query_stream;
+END
+
+
+
+
+#grep LOG_END benchmark-output-raw.txt | sort | awk '//{printf ("%s %s\n" , $2, $3) }'
+
